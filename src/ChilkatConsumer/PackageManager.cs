e4ba0a72cb6f3a07463c7cce5960c65c1312a411
Gaurav <gaurav.ydv@archerirm.com>
2024-06-03 11:19:33 +0530

    Added sample app for chilkat demo with frameowrk and core applications

---
+++ src/ChilkatConsumer/PackageManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ChilkatConsumer
+{
+    public class PackageManager
+    {
+        //Create a method which uses chilkat library to zip a list of file names received as input
+        public void ZipFiles(string[] fileNames)
+        {
+            //Create a new zip object
+            Chilkat.Zip zip = new Chilkat.Zip();
+
+            //Set the zip file name
+            zip.FileName = "output.zip";
+
+            //Create a new zip file
+            zip.NewZip(zip.FileName);
+
+            //Add the files to the zip
+            foreach (string fileName in fileNames)
+            {
+                zip.AppendOneFileOrDir(fileName, false);
+            }
+
+            //Write the zip file
+            zip.WriteZipAndClose();
+        }
+    }
+}
---
+++ src/Chilkatsample/Program.cs
@@ -0,0 +1,29 @@
+﻿using ChilkatConsumer;
+
+namespace ChilkatSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("trying to zip the files");
+
+                var packageManager = new PackageManager();
+                packageManager.ZipFiles(new string[] { "file1.txt", "file2.txt" });
+
+                Console.WriteLine("Zipped the files successfully");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to zip the files");
+
+                Console.WriteLine(e);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
